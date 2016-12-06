00e698e26c1069f6423a27f3dfdad6f3fac3b5d1
McCall Alexander <mccallalexander96@gmail.com>
2016-12-06 10:46:45 -0700

    saving

--- app/js/app-routes.js
+++ app/js/app-routes.js
@@ -17,21 +17,17 @@ let app = angular.module('drone-tracker')
                 url: '/search',
                 template: '<search></search>' 
             }) 
-<<<<<<< HEAD:app/app-routes.js
             .state({
                 name: 'map',
                 url: '/map',
                 template: '<map></map>' 
             })
-  
-=======
              .state({
                 name: 'learn',
                 url: '/learn',
                 template: '<learn></learn>' 
             })
            
->>>>>>> 2afe00c3d26697b7e22746a15939fc298f708b49:app/js/app-routes.js
 
         })
         
---
+++ dist/js/app.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('drone-tracker', ['ui.router', 'ngSanitize', 'angularUtils.directives.dirPagination', 'nvd3', 'ui.bootstrap']);
