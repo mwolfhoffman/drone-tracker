54bbf40cf927bb1663bdc849315b7ef23fe45015
McCall Alexander <mccallalexander96@gmail.com>
2016-12-06 10:46:45 -0700

    saving

---
+++ js/app-routes.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+
+    var app = angular.module('drone-tracker').config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider.state({
+            name: 'home',
+            url: '/home',
+            template: '<home></home>'
+        }).state({
+            name: 'search',
+            url: '/search',
+            template: '<search></search>'
+        }).state({
+            name: 'map',
+            url: '/map',
+            template: '<map></map>'
+        }).state({
+            name: 'learn',
+            url: '/learn',
+            template: '<learn></learn>'
+        });
+    });
+})();
